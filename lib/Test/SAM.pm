5bb5c3497b811da8a1b03c85da71a6f58663a76b
Stevan Little <stevan.little@gmail.com>
2022-06-15 00:52:41 +0200

    forgot the actual test sam module

---
+++ lib/Test/SAM.pm
@@ -0,0 +1,21 @@
+package Test::SAM;
+
+use v5.24;
+use warnings;
+use experimental 'signatures', 'postderef';
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:STEVAN';
+
+our $TESTING;
+BEGIN {
+    $TESTING = 1;
+}
+
+1;
+
+__END__
+
+=pod
+
+=cut
