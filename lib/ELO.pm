dd529169f34f5446f7e571beec2fd736e0e7532a
Stevan Little <stevan.little@gmail.com>
2022-06-22 21:05:26 +0200

    notes on public functions

--- lib/ELO.pm
+++ lib/ELO.pm
@@ -28,6 +28,47 @@ our @EXPORT = (
     @ELO::Debug::EXPORT,
 );
 
+=pod
+
+# Building Actors
+
+actor match
+
+# Flow Controls
+
+ident sequence parallel
+
+# Loop
+
+loop msg
+TICK
+PID CALLER
+
+# PROCESSES
+
+proc::exists
+proc::lookup
+proc::spawn
+proc::despawn
+
+# SIGNALS
+
+sig::kill
+sig::waitpid
+sig::timer
+
+# I/O
+
+err::log
+err::logf
+
+out::print
+out::printf
+
+in::read
+
+=cut
+
 1;
 
 __END__
