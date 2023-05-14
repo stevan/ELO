4f1ea43f3f02508ae30eb7654b06231c608349a0
Stevan Little <stevan.little@gmail.com>
2023-05-14 04:07:59 +0200

    more particles stuff

---
+++ examples/particles-again.pl
@@ -0,0 +1,166 @@
+#!perl
+
+use v5.36;
+use experimental 'builtin';
+
+use Data::Dumper;
+
+use ELO::Loop;
+use ELO::Actors    qw[ match receive ];
+use ELO::Timers    qw[ :timers :tickers ];
+use ELO::Types     qw[ :core :events ];
+
+use Term::ANSIColor qw[ colored uncolor ];
+
+use constant QUIET => $ENV{QUIET} // 0;
+
+use ELO::Util::TermDisplay;
+
+my $term = ELO::Util::TermDisplay->new;
+
+my $NUM_PARTICLES = shift(@ARGV) // 100;
+my $WAIT_TIME     = shift(@ARGV) // 30;
+my $GRAVITY       = shift(@ARGV) // 5;
+
+event *eTick;
+
+sub Particle ($id, $window, $capture_window) {
+
+    state $height = $window->height;
+    state $width  = $window->width;
+
+     # ○ ● ◎ ◍
+
+    my $sprite = '●'; # chr(int(rand( 126 - 32 )) + 32);
+    my $trail  = '○'; # chr(int(rand( 126 - 32 )) + 32);
+
+    my $color = 'ansi'.($id % 255);
+
+    my $up_down    = 1;                         # 1 = DOWN,  0 = UP
+    my $right_left = (int(rand(10)) % 2) ? 1 : 0; # 1 = RIGHT, 0 = LEFT
+
+    my $x_weight = rand;
+    my $y_weight = rand;
+
+    my $x_seed = rand;
+    my $y_seed = rand;
+
+    my $x = rand($height);
+    my $y = rand($width);
+
+    my $mass = rand() / $GRAVITY;
+
+    my $friction = 0;
+
+    receive +{
+        *eTick => sub ($this) {
+
+            my ($old_x, $old_y) = ($x, $y);
+
+            my $_x = ($up_down    ? $x_seed : -$x_seed );
+            my $_y = ($right_left ? $y_seed : -$x_seed );
+
+            $x += $_x * $x_weight;
+            $y += $_y * $y_weight;
+
+            if ($x >= $height) {
+                $x = $height;
+                $up_down = !$up_down;
+            }
+            elsif ($x <= $friction) {
+                $x = $friction;
+                $up_down = !$up_down;
+            }
+
+            if ($y >= $width) {
+                $y = $width;
+                $right_left = !$right_left;
+            }
+            elsif ($y <= 0) {
+                $y = 0;
+                $right_left = !$right_left;
+            }
+
+            $friction += $mass;
+
+            unless (QUIET) {
+                $window->put_at( colored( $trail,  'dark '.$color), $old_x, $old_y )
+                       ->put_at( colored( $sprite, 'bold '.$color ), $x, $y )
+                       ->hide_cursor
+                unless $old_x == $x && $old_y == $y;
+            }
+
+            if ( $friction >= $window->height ) {
+                $window->put_at( colored( $trail,  'dark '.$color), $x, $y );
+                $capture_window->put_at(
+                    colored( $sprite, 'bold '.$color ),
+                    (int($id / $width) % ($capture_window->height+1)),
+                    ($id % $width)
+                ) unless QUIET;
+
+                $this->exit(0);
+            }
+        }
+    }
+}
+
+sub init ($this, $msg) {
+
+    my @ps; # set of particles
+    my $i1; # animation interval
+    my $i2; # fps/gc interval
+    my $t1; # total wait timer
+
+    $term->clear_screen( with_markers => 1 );
+
+    my $particle_window  = $term->create_window( 3, 5, $term->term_height-20, $term->term_width-10 );
+    my $capture_window   = $term->create_window(
+        ($particle_window->height + 6),
+        5,
+        ($term->term_height - ($particle_window->height + $particle_window->top + 6)),
+        $term->term_width - 10
+    );
+    my $frame_cnt_window = $term->create_window( 0, 0, 1, 15 );
+    my $fps_window       = $term->create_window( 0, 15, 1, 15 );
+    my $alive_window     = $term->create_window( 0, 30, 1, 15 );
+
+    @ps = map {
+        $this->spawn( Particle( $_, $particle_window, $capture_window ) )
+    } 1 .. $NUM_PARTICLES;
+
+    $particle_window->draw_window;
+    $capture_window->draw_window;
+
+    my $frame = 0;
+    $i1 = interval( $this, 0.013, sub {
+        $frame_cnt_window->put_at((sprintf 'frame: %05d', $frame++ ), 0, 0 );
+        $this->send( $_, [ *eTick ] ) foreach @ps;
+    });
+
+    my $fps_marker = 0;
+    $i2 = interval( $this, 0.985, sub {
+        unless (@ps = grep $_->is_alive, @ps) {
+            cancel_timer( $this, $_ ) for $i1, $i2;
+            cancel_timer( $this, $t1 );
+            $term->go_to($term->term_height+1, 0);
+            $this->exit(0);
+        }
+
+        $fps_window->put_at((sprintf '~%3d fps', $frame - $fps_marker), 0, 0 );
+        $alive_window->put_at((sprintf 'alive: %09d' => scalar @ps), 0, 0 );
+        $fps_marker = $frame;
+    });
+
+    $t1 = timer( $this, $WAIT_TIME, sub {
+        cancel_timer( $this, $_ ) for $i1, $i2;
+        $this->exit(0);
+    });
+
+    $frame_cnt_window->put_at((sprintf 'frame: %05d', $frame ), 0, 0 );
+    $fps_window->put_at((sprintf '~%3d fps', $fps_marker), 0, 0 );
+    $alive_window->put_at((sprintf 'alive: %09d' => scalar @ps), 0, 0 );
+}
+
+ELO::Loop->run( \&init );
+
+1;
--- lib/ELO/Util/TermDisplay.pm
+++ lib/ELO/Util/TermDisplay.pm
@@ -1,17 +1,18 @@
 package ELO::Util::TermDisplay;
 use v5.36;
-use warnings;
-use experimental 'signatures', 'postderef';
 
 use POSIX;
 use Term::Cap;
-use Term::ReadKey qw[ GetTerminalSize ReadKey ReadMode ];
+use Term::ReadKey   qw[ GetTerminalSize ReadKey ReadMode ];
+use Term::ANSIColor qw[ colored uncolor ];
 
 $|++;
 
-use parent 'UNIVERSAL::Object';
+use parent 'UNIVERSAL::Object::Immutable';
 use slots (
     # private
+    _fh        => sub { \*STDOUT },
+    _windows   => sub { +[] },
     _term_cap  => sub {},
     _term_size => sub {},
 );
@@ -25,7 +26,7 @@ sub BUILD ($self, $) {
     my $tc = Term::Cap->Tgetent({ TERM => undef, OSPEED => $ospeed });
 
     # require the following capabilities
-    $tc->Trequire(qw/cl cd ce cm co li/);
+    $tc->Trequire(qw/cl cd ce cm ho DO do IC co li/);
 
     $self->{_term_cap}  = $tc;
     $self->{_term_size} = [ GetTerminalSize() ];
@@ -34,8 +35,20 @@ sub BUILD ($self, $) {
 sub term_width  ($self) { $self->{_term_size}->[0] }
 sub term_height ($self) { $self->{_term_size}->[1] }
 
-sub clear_screen ($self) {
+# clearing the screen
+
+sub clear_screen ($self, %args) {
     $self->{_term_cap}->Tputs('cl', 1, *STDOUT);
+
+    if ( $args{with_markers} ) {
+        foreach my $x ( 1 .. $self->term_height-1 ) {
+            $self->go_to( $x, 0 )->put_string( colored((join '' => map {
+                ($_ % 5) == 0 ? '+' : '-'
+            } (0 .. $self->term_width-1)), 'grey5') );
+            $self->go_to( $x, 0 )->put_string( colored((sprintf '%02d', $x), 'dark cyan') );
+        }
+    }
+
     $self;
 }
 
@@ -44,33 +57,158 @@ sub clear_line ($self) {
     $self;
 }
 
-sub clear_to_end ($self) {
+sub clear_to_end_of_screen ($self) {
     $self->{_term_cap}->Tputs('cd', 1, *STDOUT);
     $self;
 }
 
-
 # move the cursor
+
 sub go_to ($self, $line, $col) {
     $self->{_term_cap}->Tgoto('cm', $col, $line, *STDOUT);
     $self;
 }
 
-sub put_string ($self, @string) {
-    print @string;
+# output
+
+sub put_string ($self, $string) {
+    *STDOUT->print( $string );
     $self;
 }
 
 sub print_line ($self, @line) {
     $self->clear_line;
-    print @line;
+    $self->put_string( join '' => @line );
     $self;
 }
 
 sub print_line_at ($self, $line_num, @line) {
     $self->go_to( $line_num, 0 );
-    $self->put_string( @line );
+    $self->put_string( join '' => @line );
     $self;
+}
+
+# windows
+
+package ELO::Util::TermDisplay::Window::Colored {
+    use v5.36;
+
+    use Term::ANSIColor qw[ colored uncolor ];
+
+    use parent 'UNIVERSAL::Object::Immutable';
+    use slots (
+        top    => sub {},
+        left   => sub {},
+
+        height => sub {},
+        width  => sub {},
+
+        bg_color => sub {},
+        fg_color => sub {},
+
+        device => sub {},
+    );
+
+    sub BUILD ($self, $) {
+        my $h = $self->{height};
+        my $w = $self->{width};
+
+        my @background = map { join '' => (' ' x $w) } 0 .. $h;
+
+        my $line = 0;
+        foreach (@background) {
+            $self->put_at( colored( $_, 'on_'.$self->{bg_color} ),  ++$line, 0 );
+        }
+    }
+
+    sub hide_cursor ($self) { $self->{device}->go_to( 0, 1000 ) }
+
+    sub put_at ($self, $string, $line=0, $col=0) {
+        # TODO: check that length is not longer than window-width
+        $line += $self->{top};
+        $col  += $self->{left};
+        # TODO: check out of bounds ...
+        $self->{device}->go_to( $line, $col );
+        $self->{device}->put_string( colored( $string, $self->{fg_color} .'on_'.$self->{bg_color} ) );
+        $self;
+    }
+}
+
+sub create_colored_window ($self, $top, $left, $height, $width, $bg_color, $fg_color) {
+    ELO::Util::TermDisplay::Window->new(
+        device   => $self,
+        top      => $top,
+        left     => $left,
+        height   => $height,
+        width    => $width,
+        bg_color => $bg_color,
+        fg_color => $fg_color,
+    )
+}
+
+package ELO::Util::TermDisplay::Window {
+    use v5.36;
+
+    use Term::ANSIColor qw[ colored uncolor ];
+
+    use parent 'UNIVERSAL::Object::Immutable';
+    use slots (
+        top    => sub {},
+        left   => sub {},
+
+        height => sub {},
+        width  => sub {},
+
+        device => sub {},
+    );
+
+    sub top    ($self) { $self->{top}  }
+    sub left   ($self) { $self->{left} }
+
+    sub height ($self) { $self->{height} }
+    sub width  ($self) { $self->{width}  }
+
+    sub draw_window ($self) {
+        my $h = $self->{height};
+        my $w = $self->{width};
+
+        my $corner = '+';
+        my $edge   = '|';
+
+        my $top  = join '' => ('-' x ($w+1));
+        my $side = join '' => (' ' x ($w+1));
+
+        $self->put_at( "${corner}${top}${corner}", -1, -1 );
+        foreach my $x ( 0 .. $h ) {
+            $self->put_at( "${edge}${side}${edge}", $x, -1);
+        }
+        $self->put_at( "${corner}${top}${corner}", $h+1, -1 );
+    }
+
+    sub hide_cursor ($self) { $self->{device}->go_to( 0, 1000 ) }
+
+    sub put_at ($self, $string, $line=0, $col=0) {
+        # TODO: check that length is not longer than window-width
+        $line += $self->{top};
+        $col  += $self->{left};
+        # TODO: check out of bounds ...
+        $self->{device}->go_to( $line, $col );
+        $self->{device}->put_string( $string );
+        $self;
+    }
+}
+
+
+sub create_window ($self, $top, $left, $height, $width) { #, $bg_color, $fg_color) {
+    ELO::Util::TermDisplay::Window->new(
+        device   => $self,
+        top      => $top,
+        left     => $left,
+        height   => $height,
+        width    => $width,
+        #bg_color => $bg_color,
+        #fg_color => $fg_color,
+    )
 }
 
 1;
