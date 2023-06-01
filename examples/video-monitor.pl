de2236df17eeed66fb032adc4a5fef645c83bb0e
Stevan Little <stevan.little@gmail.com>
2023-06-01 14:06:04 +0200

    okay, making this better

--- examples/video-monitor.pl
+++ examples/video-monitor.pl
@@ -2,144 +2,146 @@
 
 use v5.36;
 use experimental 'try', 'builtin', 'for_list';
-use builtin qw[ ceil floor indexed ];
 
 use Data::Dumper;
 use Data::Dump;
 
-use Time::HiRes     qw[ sleep time ];
-use Term::ANSIColor qw[ colored ];
-use List::Util      qw[ max min ];
-use Carp            qw[ confess ];
+package MonochromeDisplay {
+    use v5.36;
+    use experimental 'try', 'builtin', 'for_list';
+    use builtin qw[ ceil floor indexed ];
 
-use ELO::Util::TermDisplay;
+    use Data::Dumper;
+    use Data::Dump;
 
-use ELO::Actors qw[ match ];
-use ELO::Types  qw[
-    :core
-    :types
-    :typeclasses
-];
+    use Time::HiRes     qw[ sleep time ];
+    use Term::ANSIColor qw[ colored ];
+    use List::Util      qw[ max min ];
+    use Carp            qw[ confess ];
 
-# ...
+    # ...
+    use POSIX;
+    use Term::Cap;
+    use Term::ReadKey qw[ GetTerminalSize ];
 
-sub raise ($err) { confess $err }
+    use constant HIDE_CURSOR  => 'vi';
+    use constant SHOW_CURSOR  => 've';
+    use constant CLEAR_SCREEN => 'cl';
+    use constant CLEAR_LINE   => 'cm';
+    use constant TO_NEXT_LINE => 'do';
 
-# ...
+    use constant PIXEL => '▀';
 
+    my sub _init_termcap {
+        my $termios = POSIX::Termios->new; $termios->getattr;
+        my $tc = Term::Cap->Tgetent({ TERM => undef, OSPEED => $termios->getospeed });
+        $tc->Trequire( HIDE_CURSOR, SHOW_CURSOR, CLEAR_SCREEN, CLEAR_LINE, TO_NEXT_LINE );
+        $tc;
+    }
 
+    sub new ($class, $width, $height, $refresh_rate) {
+        my $self = {
+            refresh => ($refresh_rate // die 'A `refresh_rate` is required'),
+            width   => ($width        // die 'A `width` is required'),
+            height  => ($height       // die 'A `height` is required'),
+            tc      => _init_termcap,
+            fh      => \*STDOUT,
+        };
+        bless $self => $class;
+    }
 
-# ...
+    sub turn_on ($self) {
+        my $fh  = $self->{fh};
+        my $tc  = $self->{tc};
 
-use POSIX;
-use Term::Cap;
-use Term::ReadKey qw[ GetTerminalSize ];
+        $tc->Tputs(HIDE_CURSOR,  1, *$fh );
+        $tc->Tputs(CLEAR_SCREEN, 1, *$fh );
 
-my sub _init_termcap {
-    my $termios = POSIX::Termios->new; $termios->getattr;
-    my $tc = Term::Cap->Tgetent({ TERM => undef, OSPEED => $termios->getospeed });
-    # require the following capabilities
-    $tc->Trequire(qw/cl ho vi ve do ce/);
-    $tc;
-}
+        $self;
+    }
 
-my sub sanitize ($x) { defined $x && $x > 23 ? 23 : ($x < 0 ? 0 : int($x)) }
+    sub turn_off ($self) {
+        my $fh  = $self->{fh};
+        my $tc  = $self->{tc};
 
-my $BLOCK = ' ';
-my $PIXEL = '▀';
+        $tc->Tputs(SHOW_CURSOR,  1, *$fh );
+        $tc->Tputs(CLEAR_SCREEN, 1, *$fh );
 
-type *Height => *Int;
-type *Width  => *Int;
+        $self;
+    }
 
-datatype *Display => sub {
-    case MonochromeDisplay => ( *Width, *Height );
-};
+    my sub sanitize ($x) { defined $x && $x > 23 ? 23 : ($x < 0 ? 0 : int($x)) }
 
-typeclass[*Display] => sub {
+    sub run_shader ($self, $shader) {
+        my $fh  = $self->{fh};
+        my $tc  = $self->{tc};
 
-    state $tc;
-    state @buffer;
+        # FIXME: respect previously set singal
+        # but not really urgent now
+        $SIG{INT} = sub { $self->turn_off; exit(0) };
 
-    method turn_on => {
-        MonochromeDisplay => sub ($w, $h) {
-            $tc = _init_termcap;
-            $tc->Tputs('vi', 1, *STDOUT);
-            $tc->Tputs('cl', 1, *STDOUT);
+        my $ticks    = 0;
+        my @row_idxs = (0 .. ($self->{height}-1));
+        my @col_idxs = (0 .. ($self->{width} -1));
+        my @buffer   = ( map { ' ' x $self->{width} } @row_idxs );
 
-            @buffer = ( map { ' ' x $w } 0 .. $h );
-        }
-    };
+        #  fps | time in milliseconds
+        # -----+---------------------
+        #  60  |  0.01667
+        #  50  |  0.02000
+        #  30  |  0.03333
+        #  25  |  0.04000
+        #  10  |  0.10000
 
-    method turn_off => {
-        MonochromeDisplay => sub ($w, $h) {
-            $tc->Tputs('ve', 1, *STDOUT) if $tc;
-            $tc->Tputs('cl', 1, *STDOUT) if $tc;
-        }
-    };
+        my $refresh = $self->{refresh};
+        my $timing  = 1 / $refresh;
 
-    method render_frame => sub ($d, $shader) {
+        do {
+            my $start = time;
 
-        match [ *Display, $d ] => {
-            MonochromeDisplay => sub ($w, $h) {
-                my @frame;
-                foreach my ($x1, $x2) ( 0 .. $h-1 ) {
-                    my @row;
-                    foreach my $y ( 0 .. $w-1 ) {
-                        push @row => colored(
-                            $PIXEL,
-                            join ' ' => (
-                                   'grey'.sanitize($shader->( $x1, $y )),
-                                'on_grey'.sanitize($shader->( $x2, $y )),
-                            )
-                        );
-                    }
-                    push @frame => join '' => @row;
-                }
-
-                $tc->Tgoto('cm', 0, 0, *STDOUT);
-                foreach my $i ( 0 .. $#frame ) {
-                    #$tc->Tputs('ce', 1, *STDOUT);
-                    #die if $frame[$i] eq $buffer[$i];
-                    print $frame[$i]#.('-' x $i)
-                        if $frame[$i] ne $buffer[$i];
-                    $tc->Tputs('do', 1, *STDOUT);
-                }
-                #$tc->Tgoto('cm', 0, 0, *STDOUT);
-
-                @buffer = @frame;
+            my @frame;
+            foreach my ($x1, $x2) ( @row_idxs ) {
+                push @frame => join '' => map {
+                    colored(PIXEL,
+                           'grey'.sanitize($shader->( $x1, $_, $ticks )).' '.
+                        'on_grey'.sanitize($shader->( $x2, $_, $ticks ))
+                    )
+                } @col_idxs;
             }
-        }
-    };
 
-};
+            my $rows_rendered = 0;
+            $tc->Tgoto(CLEAR_LINE, 0, 0, *$fh);
+            foreach my $i ( 0 .. $#frame ) {
+                if ( $frame[$i] ne $buffer[$i] ) {
+                    print $frame[$i];
+                    $rows_rendered++;
+                }
+                $tc->Tputs(TO_NEXT_LINE, 1, *$fh);
+            }
 
-my $d = MonochromeDisplay( 80, 60 );
+            @buffer = @frame;
 
-my $start = time;
-my $tick  = 0;
+            my $dur = time - $start;
 
-sub print_stats {
-    my $duration = time - $start;
-    printf 'elapsed: %.05f frames: %05d fps: %.02f' => $duration, $tick, (($duration / $tick) * 1000);
+            #sleep( $timing - $dur ) if $dur < $timing;
+
+            # my $fps = 1 / $timing + $dur;
+            my $fps = 1 / $dur;
+            printf 'tick: %05d | lines-drawn: %03d | fps: %.03f | time(ms): %.05f' => $ticks, $rows_rendered, $fps, $dur;
+
+        } while ++$ticks;
+    }
 }
 
-$d->turn_on;
-$SIG{INT} = sub {
-    $d->turn_off;
-    print_stats;
-    exit(0);
-};
+my $d = MonochromeDisplay->new( 120, 60, 120 )
+            ->turn_on
+            ->run_shader(sub ($x, $y, $t) { $x * ($t / 1000) });
 
-while (++$tick) {
-    $d->render_frame(sub ($x, $y) { $x * ($tick / 10) });
-    print_stats;
-    #say $tick;
-    sleep(0.03);
-}
 
 1;
 
 __END__
+
 
 
 
