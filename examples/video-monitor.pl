4532c0f22e575669f0272dfc6b87aa7b2a059cf1
Stevan Little <stevan.little@gmail.com>
2023-06-03 14:08:19 +0200

    removed Termcap stuff, not needed, .. and a bunch more tweaks. it is in a good state so I will commit for now and think on what to do next

--- examples/video-monitor.pl
+++ examples/video-monitor.pl
@@ -7,116 +7,105 @@ use Data::Dumper;
 
 package VideoDisplay {
     use v5.36;
-    use experimental 'try', 'builtin', 'for_list';
-    use builtin qw[ ceil ];
+    use experimental 'for_list';
 
-    use Data::Dumper;
     use Time::HiRes qw[ sleep time ];
 
     $|++;
 
-    # TODO: remove these ... we can just
-    # wing it and see if the terminal
-    # can handle it, why not, #YOLO
-    use POSIX;
-    use Term::Cap;
+    use constant HIDE_CURSOR  => "\e[?25l";
+    use constant SHOW_CURSOR  => "\e[?25h";
+    use constant HOME_CURSOR  => "\e[0;0H";
+    use constant CLEAR_SCREEN => "\e[0;0H;\e[0J";
+    use constant RESET        => "\e[0m";
 
-    use constant HIDE_CURSOR  => 'vi';
-    use constant SHOW_CURSOR  => 've';
-    use constant CLEAR_SCREEN => 'cl';
+    use constant PIXEL        => '▀';
+    use constant PIXEL_FORMAT => "\e[38;2;%d;%d;%d;48;2;%d;%d;%d;m".PIXEL;
 
-    use constant PIXEL => '▀';
-
-    my sub _init_termcap {
-        my $termios = POSIX::Termios->new; $termios->getattr;
-        my $tc = Term::Cap->Tgetent({ TERM => undef, OSPEED => $termios->getospeed });
-        $tc->Trequire( HIDE_CURSOR, SHOW_CURSOR, CLEAR_SCREEN );
-        $tc;
-    }
-
-    sub new ($class, $width, $height, $refresh_rate) {
+    sub new ($class, $width, $height, $refresh) {
         my $self = {
-            refresh => ($refresh_rate // die 'A `refresh_rate` is required'),
-            width   => ($width        // die 'A `width` is required'),
-            height  => ($height       // die 'A `height` is required'),
-            tc      => _init_termcap,
-            fh      => \*STDOUT,
+            refresh   => ($refresh // die 'A `refresh` is required'),
+            width     => ($width   // die 'A `width` is required'),
+            height    => ($height  // die 'A `height` is required'),
+            # ...
+            _row_idxs => [ 0 .. ($height-1) ],
+            _col_idxs => [ 0 ..  ($width-1) ],
+            _timing   => 0,
         };
+
+        # calculate a reasonable refresh rate
+        if ($refresh) {
+            my $bias  = 0.0999999999;
+               $bias -= ($refresh - 60) * 0.001 if $refresh > 60;
+
+            my $timing  = (1 / $refresh);
+               $timing -= ($timing * $bias);
+
+            $self->{_timing} = $timing;
+        }
+
         bless $self => $class;
     }
 
     sub turn_on ($self) {
-        my $fh  = $self->{fh};
-        my $tc  = $self->{tc};
-
-        $tc->Tputs(HIDE_CURSOR,  1, *$fh );
-        $tc->Tputs(CLEAR_SCREEN, 1, *$fh );
-
+        print HIDE_CURSOR;
+        print CLEAR_SCREEN;
         $self;
     }
 
     sub turn_off ($self) {
-        my $fh  = $self->{fh};
-        my $tc  = $self->{tc};
-
-        $tc->Tputs(SHOW_CURSOR,  1, *$fh );
-        $tc->Tputs(CLEAR_SCREEN, 1, *$fh );
-
+        print SHOW_CURSOR;
+        print CLEAR_SCREEN;
+        print RESET;
         $self;
     }
 
     sub run_shader ($self, $shader) {
         # FIXME: respect previously set singal
         # but not really urgent now
-        local $SIG{INT} = sub { print "\e[0m"; $self->turn_off; exit(0) };
+        local $SIG{INT} = sub { $self->turn_off; exit(0) };
 
-        my $ticks    = 0;
-        my @row_idxs = (0 .. ($self->{height}-1));
-        my @col_idxs = (0 .. ($self->{width} -1));
+        my @row_idxs = $self->{_row_idxs}->@*;
+        my @col_idxs = $self->{_col_idxs}->@*;
 
-        #  fps | time in milliseconds
-        # -----+---------------------
-        #  120 | 0.00833
-        #  100 | 0.01000
-        #   60 | 0.01667
-        #   50 | 0.02000
-        #   30 | 0.03333
-        #   25 | 0.04000
-        #   10 | 0.10000
+        #use Data::Dumper;
+        #die Dumper [
+        #    (join ', ' => @row_idxs),
+        #    (join ', ' => @col_idxs),
+        #    '---',
+        #    (join ', ' => $self->{_row_idxs}->@*),
+        #    (join ', ' => $self->{_col_idxs}->@*),
+        #];
 
-        my $timing  = 0;
+        my $ticks   = 0;
         my $refresh = $self->{refresh};
-
-        if ($refresh) {
-            my $bias  = 0.0999999999;
-               $bias -= ($refresh - 60) * 0.001 if $refresh > 60;
-
-            $timing  = (1 / $refresh);
-            $timing -= ($timing * $bias);
-        }
+        my $timing  = $self->{_timing};
 
         my ($start,
             $dur, $dur_acc, $dur_actual_acc,
             $fps, $avg_fps, $actual_fps) =
-            (0,
-                0,0,0,
-                0,0,0);
+            (0,0,0,0,0,0,0);
 
-        do {
-            $start = time;
-
-            print "\e[0;0H";
+        my $shader_harness = sub {
             foreach my ($x1, $x2) ( @row_idxs ) {
                 foreach my $y ( @col_idxs ) {
-                    printf(
-                        ("\e[38;2;%d;%d;%d;48;2;%d;%d;%d;m".PIXEL),
-                            $shader->( $x1, $y, $ticks ),
-                            $shader->( $x2, $y, $ticks )
+                    printf( PIXEL_FORMAT,
+                        $shader->( $x1, $y, $ticks ),
+                        $shader->( $x2, $y, $ticks ),
                     );
                 }
                 say '';
             }
-            print "\e[0m";
+        };
+
+        my $harness = $shader_harness;
+
+        do {
+            $start = time;
+
+            print HOME_CURSOR;
+            $harness->();
+            print RESET;
 
             $dur      = time - $start;
             $fps      = 1 / $dur;
@@ -141,6 +130,7 @@ package VideoDisplay {
     }
 }
 
+
 my $FPS = $ARGV[0] // 60;
 my $W   = $ARGV[1] // 120;
 my $H   = $ARGV[2] // 60;
@@ -150,6 +140,14 @@ die "Height must be a even number, ... or reall weird stuff happens" if ($H % 2)
 my $d = VideoDisplay->new( $W, $H, $FPS )
             ->turn_on
             ->run_shader(sub ($x, $y, $t) {
+
+                return (55,55,55) if $x == 0 || $x == ($H-1);
+                return (55,55,55) if $y == 0 || $y == ($W-1);
+
+                return (0, 0, 0) if $x < 4 || $x > ($H-5);
+                return (0, 0, 0) if $y < 4 || $y > ($W-5);
+
+                #return $t, $x, $y;
 
                 my $r = ((($t / 255) % 2) == 0) ? ($t % 255) : (255 - ($t % 255));
                 my $g = $x;
@@ -178,6 +176,20 @@ my $d = VideoDisplay->new( $W, $H, $FPS )
 1;
 
 __END__
+
+#  fps | time in milliseconds
+# -----+---------------------
+#  120 | 0.00833
+#  100 | 0.01000
+#   60 | 0.01667
+#   50 | 0.02000
+#   30 | 0.03333
+#   25 | 0.04000
+#   10 | 0.10000
+
+CURSOR MOVEMENTS:
+    https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_(Control_Sequence_Introducer)_sequences
+
 
 # TODO:
 # Create Color types which will support
