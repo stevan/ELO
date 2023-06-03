5123a35333463f8e46693754e070cd8a0168610c
Stevan Little <stevan.little@gmail.com>
2023-06-03 12:07:28 +0200

    notes and such

--- examples/video-monitor.pl
+++ examples/video-monitor.pl
@@ -5,60 +5,19 @@ use experimental 'try', 'builtin', 'for_list';
 
 use Data::Dumper;
 
-# TODO:
-# Create Color types which will support
-# each of the following:
-#
-# Monochrome( 1 | 0 )
-#   1 bit (monochrome) display using the following chars
-#       ▄ ▀ █ <space>
-#   or even better, use these chars and do 4x4
-#       ▖  QUADRANT LOWER LEFT
-#       ▗  QUADRANT LOWER RIGHT
-#       ▘  QUADRANT UPPER LEFT
-#       ▙  QUADRANT UPPER LEFT AND LOWER LEFT AND LOWER RIGHT
-#       ▚  QUADRANT UPPER LEFT AND LOWER RIGHT
-#       ▛  QUADRANT UPPER LEFT AND UPPER RIGHT AND LOWER LEFT
-#       ▜  QUADRANT UPPER LEFT AND UPPER RIGHT AND LOWER RIGHT
-#       ▝  QUADRANT UPPER RIGHT
-#       ▞  QUADRANT UPPER RIGHT AND LOWER LEFT
-#       ▟  QUADRANT UPPER RIGH
-#
-# Greyscale( 0 .. 255 )
-#   8 bit greyscale display
-#       using 1/2 boxes & fg+bg colors
-#   it would be possible to "tint" this display
-#       given a base RGB value, we just dark/lighten it
-#
-# RGB( 0 .. 255, 0 .. 255, 0 .. 255 )
-#   24 bit color display
-#       using 1/2 boxes & fg+bg colors
-#       basicaly what I have below
-#   in theory we could also support lower bit-depths
-#
-# For other stuff, here is some ref:
-# https://www.w3.org/TR/xml-entity-names/025.html - Boxes
-# https://www.w3.org/TR/xml-entity-names/022.html - some lines and stuff
-# https://www.w3.org/TR/xml-entity-names/023.html - ^^
-# https://www.w3.org/TR/xml-entity-names/024.html - numbers
-# https://www.w3.org/TR/xml-entity-names/027.html - lines, boxes, arrows
-# https://www.w3.org/TR/xml-entity-names/029.html - arrows
-# line characters??
-# ╱ ╳ ╲
-#
-
 package VideoDisplay {
     use v5.36;
     use experimental 'try', 'builtin', 'for_list';
     use builtin qw[ ceil ];
 
     use Data::Dumper;
-
     use Time::HiRes qw[ sleep time ];
 
     $|++;
 
-    # ...
+    # TODO: remove these ... we can just
+    # wing it and see if the terminal
+    # can handle it, why not, #YOLO
     use POSIX;
     use Term::Cap;
 
@@ -176,7 +135,7 @@ package VideoDisplay {
             printf('frame: %05d | fps: %.02f | ~fps: %.02f | time(ms): %.03f | runtime(ms): %.03f | elapsed(ms): %.03f',
                    $ticks, $avg_fps, $actual_fps, $dur, $dur_acc, $dur_actual_acc);
 
-        } while ++$ticks; # <= 300;
+        } while ++$ticks; # <= 300; # ... for running NYTProf
 
         $self->turn_off;
     }
@@ -186,20 +145,17 @@ my $FPS = $ARGV[0] // 60;
 my $W   = $ARGV[1] // 120;
 my $H   = $ARGV[2] // 60;
 
-die "Height must be a even number" if ($H % 2) != 0;
+die "Height must be a even number, ... or reall weird stuff happens" if ($H % 2) != 0;
 
 my $d = VideoDisplay->new( $W, $H, $FPS )
             ->turn_on
             ->run_shader(sub ($x, $y, $t) {
-                #return $x, $y, $t;
 
                 my $r = ((($t / 255) % 2) == 0) ? ($t % 255) : (255 - ($t % 255));
                 my $g = $x;
                 my $b = $y;
 
-                #return 0, 0, 0 if $x == 2 || $x == $H-3;
-                #return 0, 0, 0 if $y == 2 || $y == $W-3;
-
+                # make some plaid ...
                 my $bump = 10;
                 foreach ( 6, 4, 8 ) {
                     ($r+=$bump, $g+=$bump, $b+=$bump) if ($y % $_) == 0;
@@ -207,6 +163,7 @@ my $d = VideoDisplay->new( $W, $H, $FPS )
                     $bump += ($bump < 0) ? 10 : -5;
                 }
 
+                # make sure we don't overflow ...
                 $r = 255 if $r > 255;
                 $g = 255 if $g > 255;
                 $b = 255 if $b > 255;
@@ -218,12 +175,51 @@ my $d = VideoDisplay->new( $W, $H, $FPS )
                 return $r, $g, $b;
             });
 
-
 1;
 
 __END__
 
-
+# TODO:
+# Create Color types which will support
+# each of the following:
+#
+# Monochrome( 1 | 0 )
+#   1 bit (monochrome) display using the following chars
+#       ▄ ▀ █ <space>
+#   or even better, use these chars and do 4x4
+#       ▖  QUADRANT LOWER LEFT
+#       ▗  QUADRANT LOWER RIGHT
+#       ▘  QUADRANT UPPER LEFT
+#       ▙  QUADRANT UPPER LEFT AND LOWER LEFT AND LOWER RIGHT
+#       ▚  QUADRANT UPPER LEFT AND LOWER RIGHT
+#       ▛  QUADRANT UPPER LEFT AND UPPER RIGHT AND LOWER LEFT
+#       ▜  QUADRANT UPPER LEFT AND UPPER RIGHT AND LOWER RIGHT
+#       ▝  QUADRANT UPPER RIGHT
+#       ▞  QUADRANT UPPER RIGHT AND LOWER LEFT
+#       ▟  QUADRANT UPPER RIGH
+#
+# Greyscale( 0 .. 255 )
+#   8 bit greyscale display
+#       using 1/2 boxes & fg+bg colors
+#   it would be possible to "tint" this display
+#       given a base RGB value, we just dark/lighten it
+#
+# RGB( 0 .. 255, 0 .. 255, 0 .. 255 )
+#   24 bit color display
+#       using 1/2 boxes & fg+bg colors
+#       basicaly what I have below
+#   in theory we could also support lower bit-depths
+#
+# For other stuff, here is some ref:
+# https://www.w3.org/TR/xml-entity-names/025.html - Boxes
+# https://www.w3.org/TR/xml-entity-names/022.html - some lines and stuff
+# https://www.w3.org/TR/xml-entity-names/023.html - ^^
+# https://www.w3.org/TR/xml-entity-names/024.html - numbers
+# https://www.w3.org/TR/xml-entity-names/027.html - lines, boxes, arrows
+# https://www.w3.org/TR/xml-entity-names/029.html - arrows
+# line characters??
+# ╱ ╳ ╲
+#
 
 
 
