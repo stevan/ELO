31b86534dc3d5f4455a0ce6a51f588bb85dfb804
Stevan Little <stevan.little@gmail.com>
2023-07-29 00:07:19 +0200

    tweaking stuff

--- examples/display/013-poke-wave.pl
+++ examples/display/013-poke-wave.pl
@@ -7,6 +7,7 @@ use builtin 'floor', 'ceil';
 $|++;
 
 use Time::HiRes qw[ sleep ];
+use List::Util qw[ shuffle sample ];
 
 use Data::Dumper;
 
@@ -19,7 +20,7 @@ use ELO::Graphics;
 # ...
 
 my $HEIGHT  = $ARGV[0] // 40;
-my $WIDTH   = $ARGV[1] // 90;
+my $WIDTH   = $ARGV[1] // 80;
 
 my $d = Display(
     *STDOUT,
@@ -31,6 +32,8 @@ my $Purple = Color(0.6, 0.1, 0.3);
 my $Red   = Color(0.9, 0.1, 0.1);
 my $Green = Color(0.1, 0.9, 0.1);
 my $Blue  = Color(0.1, 0.1, 0.9);
+my $Black = Color(0.0, 0.0, 0.0);
+my $Yellow = Color(1.0, 0.9, 0.0);
 
 my $Background = Color(0.3,0.3,0.3);
 
@@ -53,9 +56,27 @@ $d->clear_screen( $Background );
     my $scroller = SideScroller(
         $d,
         $d->area->inset_by( Point( 2, 2 )),
-        $Background->add( Color( 0.1, 0.1, 0.1 ) ),
+        $Background->add( Color( 0.1, 0.3, 1.0 ) ),
         sub ( $d, $x, $t ) {
-            state $w = Wave( 10, 0.03, 0 );
+            state @waves = (
+                Wave( 2, 0.065, 0 ),
+                Wave( 1, 0.050, 0 ),
+                Wave( 4, 0.010, 0 ),
+                Wave( 2, 0.075, 0 ),
+                Wave( 1, 0.050, 10 ),
+            );
+
+            my $g_to_top = Gradient(
+                Color( 0.0, 1.0, 0.0 ),
+                Color( 0.2, 0.3, 0.2 ),
+                20,
+            );
+
+            my $g_to_bottom = Gradient(
+                Color( 0.0, 0.0, 1.0 ),
+                Color( 1.0, 0.0, 0.3 ),
+                20,
+            );
 
             my %colors = (
                 cosec => $Blue,
@@ -66,13 +87,33 @@ $d->clear_screen( $Background );
                 cos   => $Green,
             );
 
-            foreach my $method (qw[ cosec tan sec cot sin cos ]) {
-                my $y = $w->$method( $t );
+            my $y = 0;
+            $y += $_->cos( $t ) foreach  @waves;
 
-                if ($y <= 18 && $y > -18) {
-                    $d->poke( Point( $x, $y )->add($shift_by), ColorPixel( $colors{$method} ));
-                    #warn "$method => $y \n" ;
+            if ($y <= 18 && $y > -18) {
+
+                my $to_bottom = int(($HEIGHT /2) - $y) - 1;
+                my $to_top    = int(($HEIGHT /2) + $y) - 2;
+
+                foreach ( 1 .. $to_bottom ) {
+                    $d->poke(
+                        Point( $x, $y + $_ )->add($shift_by),
+                        ColorPixel( $g_to_bottom->calculate_at( $_ ) )
+                    );
                 }
+
+                foreach ( 1 .. $to_top ) {
+                    $d->poke(
+                        Point( $x, $y - $_ )->add($shift_by),
+                        ColorPixel( $g_to_top->calculate_at( $_ ) )
+                    );
+                }
+
+                $d->poke( Point( $x, $y )->add($shift_by), ColorPixel( $Black ));
+                $d->poke( Point( $x, $y - 8 )->add($shift_by), ColorPixel( $Yellow ));
+                $d->poke( Point( $x, $y + 8 )->add($shift_by), ColorPixel( $Red ));
+
+                #warn "$method => $y \n" ;
             }
         }
     );
